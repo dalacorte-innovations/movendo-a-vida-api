5c5f5456ce40af4799206482dd6e8ae83c007875
GabrielDalacorte <gnunes.servico@gmail.com>
2024-10-27 00:53:49 -0300

    feat: finish

--- users/admin.py
+++ users/admin.py
@@ -6,22 +6,22 @@ from .models import User, UserReferral
 class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'username', 'full_name', 'phone', 'user_type', 'payment_made',
                     'last_payment', 'plan', 'referral_code')
-    search_fields = ('email', 'username', 'full_name', 'phone', 'payment_made',
+    search_fields = ('email', 'username', 'first_name', 'last_name', 'phone', 'payment_made',
                      'last_payment', 'plan', 'referral_code')
     list_filter = ('user_type', 'payment_made', 'last_payment', 'plan')
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('full_name', 'phone', 'image', 'username', 'referral_code')}),
+        ('Personal info', {'fields': ('full_name', 'phone', 'image', 'username', 'referral_code')}), 
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}), 
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-        ('Additional info', {'fields': ('user_type', 'payment_made', 'last_payment', 'plan', 'stripe_customer_id')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}), 
+        ('Additional info', {'fields': ('user_type', 'payment_made', 'last_payment', 'plan', 'stripe_customer_id')}), 
     )
 
     readonly_fields = ('date_joined', 'last_login')
 
     def full_name(self, obj):
-        return obj.full_name
+        return f"{obj.first_name} {obj.last_name}"
 
     full_name.short_description = 'Full Name'
     full_name.admin_order_field = 'full_name'
